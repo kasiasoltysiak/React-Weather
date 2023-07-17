e42ce50ce7603544ec4eafc6e73e5b7cd4c3d1cf
kasiasoltysiak <kat.soltysiak@gmail.com>
2023-07-17 17:11:00 +0200

    basic css and footer link added

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div>
       <h1>Current weather in...</h1>
       <Weatherbox />
+      </div>
+      <footer>
+      Coded open-source by Kat Soltysiak and hosted on <a href="https://github.com/kasiasoltysiak/React-Weather" target="_blank" rel="noreferrer">GitHub</a>.
+      </footer>
     </div>
   );
 }
